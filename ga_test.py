d684b7eb3906e582a47029033c7d286e22ec7d20
shinyeop <shinyeop1224@gmail.com>
2025-02-20 15:23:17 +0900

    about ga4 with streamlit conection

--- ga_test.py
+++ ga_test.py
@@ -1,41 +1,12 @@
 import streamlit as st
 from streamlit_modal import Modal
 import pandas as pd
-from bs4 import BeautifulSoup
-import shutil
-import pathlib
-import logging
+import streamlit.components.v1 as components
 
-def add_analytics_tag():   
-    analytics_js = """
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H96KS0SE20"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-H96KS0SE20');
-    </script>
-    <div id="G-H96KS0SE20"></div>
-    """
-    analytics_id = "G-H96KS0SE20"
-
-    
-    # Identify html path of streamlit
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=analytics_id): # if id not found within html file
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  # backup recovery
-        else:
-            shutil.copy(index_path, bck_index)  # save backup
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + analytics_js) 
-        index_path.write_text(new_html) # insert analytics tag at top of head
-
-add_analytics_tag()
+# Include Google Analytics tracking code
+with open("google_analytics.html", "r") as f:
+    html_code = f.read()
+    components.html(html_code, height=0)
 
 # modal 객체 생성 (키와 제목 설정)
 modal = Modal(key="boot_modal", title="자세한 정보 보기")
