bf36dccf2794d63dc5f265e9029c4b9af6641d4b
shinyeop <shinyeop1224@gmail.com>
2025-03-19 14:35:13 +0900

    Update footer email code

--- app.py
+++ app.py
@@ -135,6 +135,10 @@ functions = {
     "Matching":pg.show_matching
 }
 
+footer = st.empty()
+footer.markdown("---\n📩 문의: [your_email@example.com](mailto:shinyeop1224@gmail.com)")
+
+
 # 선택한 페이지의 함수를 호출하여 해당 페이지 내용 표시
 go_to = functions.get(page)
 if go_to:
